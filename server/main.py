f89a417ca3c541e7473977380162b35fe9a2bf5e
pro-extremecoder <vladislavmustyatsa@gmail.com>
2020-09-15 21:42:08 +0300

    After adding eventlet.monkey_patch()

--- server/main.py
+++ server/main.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, session, request
 from flask import url_for, redirect, flash
 from flask_socketio import SocketIO, emit
